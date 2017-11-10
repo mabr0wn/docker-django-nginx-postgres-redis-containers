5ba267d845af884961af4a783f5bc34b26bf3357
Matthew Brown <mattd429@gmail.com>
2017-11-10 10:22:01 -0500

    Update test_enviroment_settings.py

--- web/tests/test_enviroment_settings.py
+++ web/tests/test_enviroment_settings.py
@@ -1,1 +1,21 @@
+"""Test Enviromental setting are handled properly."""
+
+
+import os
+import importlib
+from unittest.mock import patch
+# from django.test import TestCase
+# from unittest import skip
+"""
+We have to use tools outside of Django, when it initializes 
+it's too late to change enviroment variables
+"""
+from unittest import TestCase, main
+
+class DebugSettingTest(TestCase):
+    """Test if setting DEBUG is handled properly."""
+    
+    _variants = {
+        True: ('Yes', 'YES', 'Y', 'TRUE', 'tRUE')
+    }
 
