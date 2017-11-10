11975001ae7a7bf6165ee24fc7fd61b2eb9597f8
Matthew Brown <mattd429@gmail.com>
2017-11-10 11:13:52 -0500

    Update test_enviroment_settings.py
    check Debug mode.

--- web/tests/test_enviroment_settings.py
+++ web/tests/test_enviroment_settings.py
@@ -16,6 +16,24 @@ class DebugSettingTest(TestCase):
     """Test if setting DEBUG is handled properly."""
     
     _variants = {
-        True: ('Yes', 'YES', 'Y', 'TRUE', 'tRUE')
+        True: ('Yes', 'YES', 'Y', 'TRUE', 'tRUE', 'true', 'On'),
+        
+        False: ('No', 'nO', 'N', 'n', 'false', 'False', 'off', 'oFF'),
     }
+    env_var_debug = 'DEBUG'
+    
+    def test_debug_setting(self):
+        """Check if config accepts enviroment variable DEBUG and sets it."""
+        from django_docker import settings
+        for result, words in self._variants.items():
+            for word in words:
+                # print(word, result)
+                with patch.dict('os.environ', {self.env_var_debug: word}):
+                    importlib.reload(settings)
+                    assert self.env_var_debug in os.environ
+                    self.assertEqual(settings.DEBUG, result)
+                assert self.env_var_debug not in os.environ # should be True
+                
+if __name__ == '__main__':
+    main()
 
