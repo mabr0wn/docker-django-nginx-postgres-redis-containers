c358e0e1f261e8bdac0961114c5f62aff6725be4
Matthew Brown <mattd429@gmail.com>
2017-11-12 12:11:57 -0500

    Update urls.py
    adding our urls.

--- web/django_docker/urls.py
+++ web/django_docker/urls.py
@@ -1,1 +1,8 @@
+from django.conf.urls import include, url
+from django.contrib import admin
 
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('django_docker.apps.project.urls')),
+
+]
