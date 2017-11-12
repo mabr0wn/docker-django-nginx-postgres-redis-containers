bc0a4c1545455be0d989ff8984f32def6f0708a2
Matthew Brown <mattd429@gmail.com>
2017-11-12 12:23:51 -0500

    Update views.py
    what we are writing here is we want to tell the view that our host will be Redis for our counter to work properly, if the method is a post of data on the website then create a new item with the text that the end user has produced and return it with a redirect.  we will have items be equal to all items that way we can display all our text inputs on the homepage,  then we define our counter by using redis.incr which will display a number of times a end user has visited the page.

--- web/django_docker/apps/project/views.py
+++ web/django_docker/apps/project/views.py
@@ -1,1 +1,14 @@
+from django.shorcuts import render, redirect
+from .models import item
+from redis import Redis
 
+
+redis = Redis(host='redis', port=6379)
+
+def home(request):
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+    items = Item.objects.all()
+    counter = redis.incr('counter')
+    return render(request, 'home.html', {'items': items, 'counter': counter})
