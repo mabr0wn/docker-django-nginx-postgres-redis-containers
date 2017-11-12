f301d412c2b63108c72b3bc241c6638a3199042e
Matthew Brown <mattd429@gmail.com>
2017-11-12 12:09:19 -0500

    Update wsgi.py
    web server gateway interface.

--- web/django_docker/wsgi.py
+++ web/django_docker/wsgi.py
@@ -1,1 +1,15 @@
+"""
+WSGI config for web project.
 
+It exposes the WSGI callable as a module-level variable named ``application``,
+
+for more information on this filem see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_docker.settings")
+
+application = get_wsgi_application()
